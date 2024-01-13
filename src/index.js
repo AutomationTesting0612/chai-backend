032781a3e9414702b2bbd582434a18b9bdf65376
AutomationTesting0612 <automationtesting0612@gmail.com>
2024-01-13 17:13:59 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express";
+
+const app = express()
+
+
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.on("error",(error)=>{
+    console.log("error:", error)
+    throw error
+          })
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running at port: ${process.env.PORT}`)
+  })
+})
+.catch((err)=> {
+  console.log("MONGo FB connection Failed", err);
+})
 
 
 /*(async () => {
